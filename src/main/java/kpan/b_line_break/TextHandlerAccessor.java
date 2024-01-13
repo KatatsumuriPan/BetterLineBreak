c6ad455868cedeb41419676cfe5306b4bbe352fb
KatatsumuriPan <Katatsumuri.pan@gmail.com>
2024-01-14 00:50:49 +0900

    1.0.0リリース

---
+++ src/main/java/kpan/b_line_break/TextHandlerAccessor.java
@@ -0,0 +1,8 @@
+package kpan.b_line_break;
+
+import net.minecraft.client.font.TextHandler.WidthRetriever;
+
+public interface TextHandlerAccessor {
+
+	WidthRetriever betterLineBreak$getWidthRetriever();
+}
