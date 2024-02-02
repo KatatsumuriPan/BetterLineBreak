600bdabc850e40845e3a8b67ee41ce5ae80fc38d
KatatsumuriPan <Katatsumuri.pan@gmail.com>
2024-02-02 17:54:12 +0900

    1.12.2の開発はデフォルトブランチから1.12.2forgeへ移動しました
    (cherry picked from commit cf589bdfc7d149bddac5a4c01492b7212a7b9f9f)

--- src/main/java/kpan/b_line_break/ModReference.java
+++
@@ -1,8 +0,0 @@
-package kpan.b_line_break;
-
-public class ModReference {
-
-	public static final String CLIENT_PROXY_CLASS = ModTagsGenerated.MODGROUP + ".proxy.ClientProxy";
-	public static final String COMMON_PROXY_CLASS = ModTagsGenerated.MODGROUP + ".proxy.CommonProxy";
-
-}
--- src/main/java/kpan/b_line_break/proxy/CommonProxy.java
+++
@@ -1,8 +0,0 @@
-package kpan.b_line_break.proxy;
-
-@SuppressWarnings("unused")
-public class CommonProxy {
-	public void registerOnlyClient() { }
-
-	public boolean hasClientSide() { return false; }
-}
