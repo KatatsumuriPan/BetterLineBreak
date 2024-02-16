e8d73d076afad411ed13cada2ae4f5b1109ad2d9
KatatsumuriPan <Katatsumuri.pan@gmail.com>
2024-02-16 19:35:58 +0900

    間違って消してしまっていたので復元

--- src/main/java/kpan/b_line_break/LineBreakingUtil.java
+++ src/main/java/kpan/b_line_break/LineBreakingUtil.java
@@ -276,6 +276,8 @@ public class LineBreakingUtil {
 					return false;
 				if (isStartBracket(prevChar))
 					return false;
+				if (breakIndices.contains(index))
+					return true;
 				if (!isNormalAsciiLetter(prevChar) && isNormalAsciiLetter(c))
 					return true;
 				return false;
